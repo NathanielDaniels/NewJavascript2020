cae52b12e0643ae533b31c1d8092b8c2f1f15c02
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-12-07 16:08:04 -0600

    array + for loop from memory

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-if ('hello'.length === 4) {
-  console.log('niiiice')
-} else if ('Test'.length > 6) {
-  console.log('very niiiiiice')
-} else {
-  console.log('you suck')
+// if ('hello'.length === 4) {
+//   console.log('niiiice')
+// } else if ('Test'.length > 6) {
+//   console.log('very niiiiiice')
+// } else {
+//   console.log('you suck')
+// }
+
+let colors = ['purple', 'lilac', 'violet']
+
+for (let i = 0; i > colors.length; i++) {
+  if (colors[i] === 'lilac') {
+    console.log('Lilac is a great color')
+  }
 }
