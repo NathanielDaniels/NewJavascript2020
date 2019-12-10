31e7f1add8b5402df5478871e1abb4de92ed8215
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-12-09 23:46:53 -0600

    learned slice, onto splice

--- script.js
+++ script.js
@@ -138,6 +138,13 @@ console.log(ingredients.reverse())
 //* ====================
 //! Slice
 
+let animals = ['shark', 'salmon', 'whale', 'bear', 'lizard', 'tortoise']
+
+let swimmers = animals.slice(0, 3)
+let mammals = animals.slice(2, 4)
+let reptiles = animals.slice(4)
+let quadruped = animals.slice(-3)
+
 //* ====================
 //! Splice
 
