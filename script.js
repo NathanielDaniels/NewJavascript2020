69e4b03f3e0335190a4b7f36936f49a3953b8375
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-12-31 00:00:12 -0600

    reduce

--- script.js
+++ script.js
@@ -1387,12 +1387,10 @@ const grades = [87, 64, 96, 92, 88, 99, 73, 70, 64];
 const totalGrades = grades.reduce((total, currentVal) => total + currentVal);
 
 const maxGrades = grades.reduce((max, currVal) => {
-  console.log(max);
-  if (currVal > max) return currVal;
-  return max;
+  return Math.max(max, currVal);
 });
 
-console.log("grades", totalGrades);
+// console.log("grades", totalGrades);
 
 console.log("max grade", maxGrades);
 
