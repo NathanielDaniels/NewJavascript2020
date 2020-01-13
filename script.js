df05b66c1f04275de703f0129af9bf65d73f42e6
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-01-13 11:54:53 -0600

    starting NBA Score chart. warriorsGames array complete, ready to start

--- script.js
+++ script.js
@@ -2374,8 +2374,112 @@ let js = document.getElementById("js");
 
 //!======================================
 //* removeChild & remove (lecture #149)
+
+//* Remove Child
+// const ul = document.querySelector("ul");
+// const removeMe = ul.querySelector(".special");
+// const deletedUl = ul.removeChild(removeMe);
+
+// // Take removed Ul and append to second list
+// const ulTwo = document.querySelectorAll("ul")[1];
+// ulTwo.append(deletedUl);
+
+// //* Remove
+// //? its as simple as calling .remove()
+// //! Does NOT work in IE
+// const firstLi = document.querySelector("li");
+// firstLi.remove();
 //!======================================
 //* NBA Scores Chart Pt.1 (lecture #150)
+
+const warriorsGames = [
+  {
+    awayTeam: {
+      team: "Golden State",
+      points: 119,
+      isWinner: true
+    },
+    homeTeam: {
+      team: "Houston",
+      points: 106,
+      isWinner: false
+    }
+  },
+  {
+    awayTeam: {
+      team: "Golden State",
+      points: 105,
+      isWinner: false
+    },
+    homeTeam: {
+      team: "Houston",
+      points: 127,
+      isWinner: true
+    }
+  },
+  {
+    homeTeam: {
+      team: "Golden State",
+      points: 126,
+      isWinner: true
+    },
+    awayTeam: {
+      team: "Houston",
+      points: 85,
+      isWinner: false
+    }
+  },
+  {
+    homeTeam: {
+      team: "Golden State",
+      points: 92,
+      isWinner: false
+    },
+    awayTeam: {
+      team: "Houston",
+      points: 95,
+      isWinner: true
+    }
+  },
+  {
+    awayTeam: {
+      team: "Golden State",
+      points: 94,
+      isWinner: false
+    },
+    homeTeam: {
+      team: "Houston",
+      points: 98,
+      isWinner: true
+    }
+  },
+  {
+    homeTeam: {
+      team: "Golden State",
+      points: 115,
+      isWinner: true
+    },
+    awayTeam: {
+      team: "Houston",
+      points: 86,
+      isWinner: false
+    }
+  },
+  {
+    awayTeam: {
+      team: "Golden State",
+      points: 101,
+      isWinner: true
+    },
+    homeTeam: {
+      team: "Houston",
+      points: 92,
+      isWinner: false
+    }
+  }
+];
+
+console.log(warriorsGames.points);
 //!======================================
 //* NBA Scores Chart Refactor (lecture #151)
 
