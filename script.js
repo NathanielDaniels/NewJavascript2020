bc5535fe110ae67e24d913c24ba5811becad1fb2
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-01-18 14:20:05 -0600

    Everything setup for Coin Game

--- script.js
+++ script.js
@@ -2690,6 +2690,19 @@ console.log(boxes);
 // });
 //!===============
 //* Coin Game Demo
+
+// function isTouching(a, b) {
+//   const aRect = a.getBoundingClientRect();
+//   const bRect = b.getBoundingClientRect();
+
+//   return !(
+//     aRect.top + aRect.height < bRect.top ||
+//     aRect.top > bRect.top + bRect.height ||
+//     aRect.left + aRect.width < bRect.left ||
+//     aRect.left > bRect.left + bRect.width
+//   );
+// }
+
 //!===============
 //* Form Events & PreventDefault
 //!===============
